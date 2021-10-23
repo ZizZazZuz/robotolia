28095fd1a2a6a48c56b831c9ca0af68f898d3dd8
Serious Table <srstable.gamedev@gmail.com>
2021-10-23 15:07:50 -0500

    Updated .gitignore, intial copy from serious-bot.

---
+++ anatolia-bot-2_0.js
@@ -0,0 +1,35 @@
+// Require the necessary discord.js classes
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+require('dotenv').config()
+// const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Read the events listed in the /events folder, but only if they're .js
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// Login to Discord with our client's token
+client.login(process.env.DISCORD_TOKEN);
